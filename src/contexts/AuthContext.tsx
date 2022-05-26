595df4234b83347fda87d971585aa691675dbf83
Douglas Soares <douglasmaismcpe@gmail.com>
2022-05-26 20:56:04 -0300

    Recover state of authentication

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
 import Router from "next/router";
-import { setCookie } from "nookies";
+import { setCookie, parseCookies } from "nookies";
 
 import { api } from "../services/api";
 
@@ -31,6 +31,22 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { "auth_jwt.token": token } = parseCookies();
+
+    if (token) {
+      api.get("/me").then(response => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({
+          email,
+          permissions,
+          roles
+        });
+      })
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const { data } = await api.post("sessions", {
@@ -42,7 +58,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         maxAge: 60 * 60 * 24 *30, // 30 days
         path: "/"
       });
-      
+
       setCookie(undefined, "auth_jwt.refreshToken", data.refreshToken, {
         maxAge: 60 * 60 * 24 *30, // 30 days
         path: "/"
@@ -53,6 +69,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         permission: data.permissions,
         roles: data.roles,
       });
+
+      api.defaults.headers["Authorization"] = `Bearer ${data.token}`;
 
       Router.push("/dashboard");
     } catch (error) {
