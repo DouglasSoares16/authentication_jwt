cb5e309589809a93f7e24291fba2c3aae14ec488
Douglas Soares <douglasmaismcpe@gmail.com>
2022-05-26 20:31:45 -0300

    Saving Tokens in Cookies

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState } from "react";
 import Router from "next/router";
+import { setCookie } from "nookies";
 
 import { api } from "../services/api";
 
@@ -35,6 +36,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
       const { data } = await api.post("sessions", {
         email,
         password
+      });
+
+      setCookie(undefined, "auth_jwt.token", data.token, {
+        maxAge: 60 * 60 * 24 *30, // 30 days
+        path: "/"
+      });
+      
+      setCookie(undefined, "auth_jwt.refreshToken", data.refreshToken, {
+        maxAge: 60 * 60 * 24 *30, // 30 days
+        path: "/"
       });
 
       setUser({
