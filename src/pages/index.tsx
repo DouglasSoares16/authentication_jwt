3ecfcab11646c123acf527a44d0092ea062d8fd9
Douglas Soares <douglasmaismcpe@gmail.com>
2022-05-26 19:18:21 -0300

    Created Login Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,34 @@
+import { useState, FormEvent } from "react";
+import { Flex, FormControl, FormLabel, Input, Button, Center } from "@chakra-ui/react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password
+    };
+
+    alert(data.email)
+  }
+
   return (
-    <h1>init</h1>
+    <Flex align="center" justifyContent="center" margin="auto" maxW={500} h="100vh">
+      <FormControl as="form" onSubmit={handleSubmit}>
+        <FormLabel htmlFor="email">Email</FormLabel>
+        <Input id="email" type="email" value={email} onChange={e => setEmail(e.target.value)} />
+
+        <FormLabel mt="4" htmlFor="password">Password</FormLabel>
+        <Input id="password" type="password" value={password} onChange={e => setPassword(e.target.value)}  />
+
+        <Center mt="4">
+          <Button type="submit" colorScheme='blue'>Login</Button>
+        </Center>
+      </FormControl>
+    </Flex>
   )
 }
