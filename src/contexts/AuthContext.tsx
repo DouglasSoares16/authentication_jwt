034efe1d9c0dafaedaa65ca6ddc5587c6052ab8f
Douglas Soares <douglasmaismcpe@gmail.com>
2022-05-26 20:15:56 -0300

    Saving User Data

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,6 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+import Router from "next/router";
+
 import { api } from "../services/api";
 
 type SignInCredentials = {
@@ -9,32 +11,46 @@ type SignInCredentials = {
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
   isAuthenticated: boolean;
+  user: User;
 };
 
 type AuthProviderProps = {
   children: ReactNode;
 };
 
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
-      const response = await api.post("sessions", {
+      const { data } = await api.post("sessions", {
         email,
         password
       });
 
-      console.log(response.data);
+      setUser({
+        email,
+        permission: data.permissions,
+        roles: data.roles,
+      });
+
+      Router.push("/dashboard");
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
