60c59ce97ed0aae8e9021c470362c923b2c0356a
Douglas Soares <douglasmaismcpe@gmail.com>
2022-05-26 21:23:27 -0300

    Realizing Refresh of Token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,43 @@
-import axios from "axios";
-import { parseCookies } from "nookies";
+import axios, { AxiosError } from "axios";
+import { parseCookies, setCookie } from "nookies";
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
   headers: {
     Authorization: `Bearer ${cookies["auth_jwt.token"]}`
+  }
+});
+
+api.interceptors.response.use(response => {
+  return response;
+}, (error: AxiosError) => {
+  if (error.response.status === 401) {
+    if (error.response.data?.code === "token.expired") {
+      cookies = parseCookies();
+
+      const { "auth_jwt.token": refreshToken } = cookies;
+
+      api.post("/refresh", {
+        refreshToken
+      }).then(response => {
+        const { token } = response.data;
+
+        setCookie(undefined, "auth_jwt.token", data.token, {
+          maxAge: 60 * 60 * 24 *30, // 30 days
+          path: "/"
+        });
+
+        setCookie(undefined, "auth_jwt.refreshToken", response.data.refreshToken, {
+          maxAge: 60 * 60 * 24 *30, // 30 days
+          path: "/"
+        });
+
+        api.defaults.headers["Authorization"] = `Bearer ${token}`;
+      })
+    } else {
+
+    }
   }
 });
